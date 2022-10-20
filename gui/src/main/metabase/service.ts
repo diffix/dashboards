c6f2accd01ca8f10c5427307fdeb31ea16716ed3
Edon Gashi <egashi@mpi-sws.org>
2022-10-20 17:43:49 +0200

    Fix merge conflict

--- gui/src/main/metabase/service.ts
+++ gui/src/main/metabase/service.ts
@@ -14,7 +14,7 @@ export function startMetabase(): PromiseWithChild<{ stdout: string; stderr: stri
 
   fs.mkdirSync(metabaseConfig.pluginsDir, { recursive: true });
 
-  return asyncExecFile('java', ['-jar', metabaseConfig.jarPath], {
+  return asyncExecFile(metabaseConfig.jarPath, [], {
     env: {
       MB_DB_TYPE: 'postgres',
       MB_DB_DBNAME: postgresConfig.metadataDatabase,
