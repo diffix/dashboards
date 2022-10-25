9d213e3d2e8fa3b9271047f07dd6107b5cbe016d
pdobacz <5735525+pdobacz@users.noreply.github.com>
2022-10-25 15:51:45 +0200

    Move data dir to appdata (.config) location

--- gui/scripts/teardown.cmd
+++ gui/scripts/teardown.cmd
@@ -4,8 +4,8 @@ setlocal
 
 set LOCAL_PG_BIN=%~dp0..\pgsql\bin
 set PATH=%PATH%;%LOCAL_PG_BIN%
-set DIFFIX_DASHBOARDS_HOME=%userprofile%\.diffix_dashboards
-set DIFFIX_DASHBOARDS_POSTGRES=%DIFFIX_DASHBOARDS_HOME%\postgres
+set DIFFIX_DASHBOARDS_DATA="%appdata%\Roaming\Diffix Dashboards\data"
+set DIFFIX_DASHBOARDS_POSTGRES="%DIFFIX_DASHBOARDS_DATA%\postgres"
 
-pg_ctl -w -D %DIFFIX_DASHBOARDS_POSTGRES% stop
-rd /s /q %DIFFIX_DASHBOARDS_HOME%
+pg_ctl -w -D "%DIFFIX_DASHBOARDS_POSTGRES%" stop
+rd /s /q "%DIFFIX_DASHBOARDS_DATA%"
